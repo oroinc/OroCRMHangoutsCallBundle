b0124965df7977f570071941a433e5424cce09b5
Igor Galayev <igalayev@oroinc.com>
2018-04-03 16:21:17 +0300

    BAP-16688: Replace form aliases by FQCN in entity configs - replace form type for use_hangout field of CalendarEvent

--- Migrations/Schema/OroHangoutsCallBundleInstaller.php
+++ Migrations/Schema/OroHangoutsCallBundleInstaller.php
@@ -14,7 +14,7 @@ class OroHangoutsCallBundleInstaller implements Installation
      */
     public function getMigrationVersion()
     {
-        return 'v1_0';
+        return 'v1_1';
     }
 
     /**
