d306939dc0d0002fb95c8da8323c600c3a7ba342
Andrey Yatsenco <ayatsenko@oroinc.com>
2018-06-25 11:41:28 +0300

    BAP-12799: Upgrade of the 3rd party dependencies to the latest stable… (phpunit) (#18583)
    * BAP-12799: Upgrade of the 3rd party dependencies to the latest stable version
     - updated phpunit deps
     - updated tests and php_cs config to be compatible with phpunit 6
     - updated tests according to BC breaks generated dev.locks
     - updated namespaces in unit tests
     - fixed PHPUnit\Framework\MockObject\Matcher\Invocation usage instead of MockBuilderion
     - replaced @before annotation with @afterInitClient
     - cs fix
     - updated namespaces

--- Tests/Unit/DependencyInjection/ConfigurationTest.php
+++ Tests/Unit/DependencyInjection/ConfigurationTest.php
@@ -5,7 +5,7 @@ namespace Oro\Bundle\HangoutsCallBundle\Tests\Unit\DependencyInjection;
 use Oro\Bundle\HangoutsCallBundle\DependencyInjection\Configuration;
 use Symfony\Component\Config\Definition\Processor;
 
-class ConfigurationTest extends \PHPUnit_Framework_TestCase
+class ConfigurationTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @dataProvider dataProviderExceptionConfigTree
